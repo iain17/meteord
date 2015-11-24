30c9903b021dc7a482a5a5a65ee7b767029823fb
Iain Munro <iain17@me.com>
2015-11-24 01:22:27 +0100

    improved phantomjs

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 cd base
-docker rmi focuswebsolutions/meteord-base
+#docker rmi focuswebsolutions/meteord-base
 docker build -t focuswebsolutions/meteord-base .
 docker push focuswebsolutions/meteord-base
 cd ../base2/
-docker rmi focuswebsolutions/meteord-base2
+#docker rmi focuswebsolutions/meteord-base2
 docker build -t focuswebsolutions/meteord-base2 .
 cd ../base3/
 docker build -t focuswebsolutions/meteord .
